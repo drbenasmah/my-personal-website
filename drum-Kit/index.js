a3e7b033a68790ca8d437105566dbed1e9eecbbe
drbenasmah <drbenasmah2016@gmail.com>
2023-06-13 00:47:42 +0200

    update

---
+++ drum-Kit/index.js
@@ -0,0 +1,56 @@
+var element = document.querySelectorAll(".drum");
+
+for (i = 0; i < element.length; i++) {
+  element[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    playSound(buttonInnerHTML);
+
+    // Add event listener to detect key presses
+    document.addEventListener("keydown", function (event) {
+      var key = event.key.toLowerCase();
+      playSound(key);
+    });
+
+    function playSound(character) {
+      switch (character) {
+        case "w":
+          var tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+
+        case "a":
+          var tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+
+        case "s":
+          var tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+
+        case "d":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+
+        case "j":
+          var crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+
+        case "k":
+          var kickBass = new Audio("sounds/kick-bass.mp3");
+          kickBass.play();
+          break;
+
+        case "l":
+          var snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+
+        default:
+          break;
+      }
+    }
+  });
+}
