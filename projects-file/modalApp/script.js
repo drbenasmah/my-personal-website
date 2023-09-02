dfdb45bc392e14b34b278c49d71c00633b0cc912
drbenasmah <drbenasmah2016@gmail.com>
2023-09-02 07:17:47 +0200

    update on modal window

--- projects-file/modalApp/script.js
+++ projects-file/modalApp/script.js
@@ -1,25 +1,32 @@
-'use strict';
-const overlay = document.querySelector('.overlay');
-const closeModalbtn = document.querySelector('.close-modal');
-const modal = document.querySelector('.modal');
-const showModalbtns = document.querySelectorAll('.show-modal');
+"use strict";
+const overlay = document.querySelector(".overlay");
+const closeModalbtn = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+const showModalbtns = document.querySelectorAll(".show-modal");
 
 const openModalFunction = function () {
-  console.log('button clicked');
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  console.log("button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModalFunction = function () {
-  console.log('close button clicked');
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
 for (let i = 0; i < showModalbtns.length; i++) {
-  showModalbtns[i].addEventListener('click', openModalFunction);
+  showModalbtns[i].addEventListener("click", openModalFunction);
 }
 
-closeModalbtn.addEventListener('click', closeModalFunction);
+closeModalbtn.addEventListener("click", closeModalFunction);
 
-overlay.addEventListener('click', closeModalFunction);
+overlay.addEventListener("click", closeModalFunction);
+
+// esc to close
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModalFunction();
+  }
+});
